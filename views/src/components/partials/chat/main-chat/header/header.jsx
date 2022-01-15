a201b1c3fa9147e3fb33520d1de33549a26b0190
dpkhang <khangphuc1@gmail.com>
2022-01-16 01:30:35 +0700

    create search message, update message, update emoji

--- views/src/components/partials/chat/main-chat/header/header.jsx
+++ views/src/components/partials/chat/main-chat/header/header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './header.scss'
 import Image from '../../avatar/avatar'
+import SearchBox from './search-message/search-message'
 import {showExtension} from '../../../../../redux/actions/extension'
 import {useSelector, useDispatch} from 'react-redux'
+import { useEffect, useState } from 'react'
+import $ from 'jquery'
 
 function Header(props) {
     
     //redux
     const isShowExtension = useSelector(state => state.extension.isShow)
     const dispatch = useDispatch()
+
+    //states
+    const [isShowSearchBox, setIsShowSearchBox]  = useState(0)
 
     const user = {
         id: '1',
@@ -21,30 +27,59 @@ function Header(props) {
 
     //handles
 
-    const handleClickShowExtension = (e)=>{
+    const handleClickShowExtension = ()=>{
         const isShow = showExtension(isShowExtension === 0 ? 1 : 0)
         dispatch(isShow)
     }
 
+    const handleShowSearchMessageBox = (isShow)=> {
+        setIsShowSearchBox(isShow)
+    }
+
+    useEffect(()=>{
+        if(isShowExtension === 1) {
+            $('.main-chat-header-wrapper .fa-bars').css('background', '#5b67ee49')
+        }else {
+            $('.main-chat-header-wrapper .fa-bars').css('background', 'none')
+        }
+    }, [isShowExtension])
+
+    useEffect(()=>{
+        if(isShowSearchBox === 1) {
+            $('.main-chat-header-wrapper .main-chat-header-box .fa-search').css('background', '#5b67ee49')
+        }else {
+            $('.main-chat-header-wrapper .main-chat-header-box .fa-search').css('background', 'none')
+        }
+    }, [isShowSearchBox])
+
     return (
-        <div className="main-chat-header-wrapper">
-            <div className="main-chat-header-receiver-info">
-                <div className="main-chat-header-receiver-avatar">
-                    <Image image={user.image}></Image>
+        <>
+            <div className="main-chat-header-wrapper">
+                <div className="main-chat-header-box">
+                    <div className="main-chat-header-receiver-info">
+                        <div className="main-chat-header-receiver-avatar">
+                            <Image image={user.image}></Image>
+                        </div>
+                        <div className="main-chat-header-receiver-text">
+                            <p className="main-chat-header-username">{name}</p>
+                            <p className="main-chat-header-active">Đang hoạt động</p>
+                        </div>
+                    </div>
+                    <div className="main-chat-header-tools">
+                        <i className="fas fa-search"  onClick={()=>setIsShowSearchBox(isShowSearchBox ? 0 : 1)}></i>
+                        <i className="fas fa-phone"></i>
+                        <i className="fas fa-video"></i>
+                        <i className="fas fa-bars" onClick={handleClickShowExtension}></i>
+                    </div>
                 </div>
-                <div className="main-chat-header-receiver-text">
-                    <p className="main-chat-header-username">{name}</p>
-                    <p className="main-chat-header-active">Đang hoạt động</p>
-                </div>
+                {
+                    isShowSearchBox ? <SearchBox onShowSearchMessageBox={handleShowSearchMessageBox}></SearchBox> : ''
+                }
+                
             </div>
-            <div className="main-chat-header-tools">
-                <i className="fas fa-search"></i>
-                <i className="fas fa-phone"></i>
-                <i className="fas fa-video"></i>
-                <i className="fas fa-bars" onClick={handleClickShowExtension}></i>
-            </div>
-        </div>
+            
+        </>
     )
 }
 
-export default Header
+export default React.memo(Header)

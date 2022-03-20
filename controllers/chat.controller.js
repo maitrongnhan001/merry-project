2e00c047553a632cc3a6e2a920fb4459ab1f7d28
dpkhang <khangphuc1@gmail.com>
2022-03-21 02:53:30 +0700

    update picture start

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -6,12 +6,13 @@ const user = require('../models/user.model')
 const getMembers = async (groupId, userId) => {
 
     let id = await group.getMembers(groupId)
+    let admin = await group.getAdminId(groupId)
     var user1, user2;
     if (userId == id[0].userId) {
-        user1 = await user.getUserId(id[0].userId)
+        user1 = await user.getUserId(admin[0].AdminId ? admin[0].AdminId : id[0].userId)
         user2 = await user.getUserId(id[1].userId)
     } else {
-        user1 = await user.getUserId(id[1].userId)
+        user1 = await user.getUserId(admin[0].AdminId ? admin[0].AdminId : id[0].userId)
         user2 = await user.getUserId(id[0].userId)
     }
 
